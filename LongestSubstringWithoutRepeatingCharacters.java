cd6a17c6395ddd7507e974977bf3f1ca1c1cf118
wanglinglong <564958390@qq.com>
2015-10-21 20:25:32 +0800

    add Longest Substring Without Repeating Characters Use LRU data structure

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,24 @@
+/**
+ * Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ * 
+ * /
+ 
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int i = 0, j = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                j++;
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+    
+        return max;
+    }
+}
--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,5 @@
 ###[1. Two Sum Problem solution O(n) time		--2015.10.19](https://github.com/w4lle/leetcode_solution/blob/master/TwoSum.java)
 
 ###[2. Add Two Numbers  						--2015.10.20](https://github.com/w4lle/leetcode_solution/blob/master/AddTwoNumbers.java)
+
+###[3. Longest Substring Without Repeating Characters Use 	LRU data structure			--2015.10.21](https://github.com/w4lle/leetcode_solution/blob/master/LongestSubstringWithoutRepeatingCharacters.java)
