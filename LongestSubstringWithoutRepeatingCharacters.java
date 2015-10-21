6de63e6283cd67275afff31a00ea84adef792f75
wanglinglong <564958390@qq.com>
2015-10-21 20:26:24 +0800

    add Longest Substring Without Repeating Characters Use LRU data structure

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,8 +1,9 @@
 /**
- * Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ * Given a string, find the length of the longest substring without repeating characters. 
+ * For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. 
+ * For "bbbbb" the longest substring is "b", with the length of 1.
  * 
- * /
- 
+ */
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int i = 0, j = 0, max = 0;
