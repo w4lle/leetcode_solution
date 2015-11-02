a35c684d93633c45acebfc5a4e70b49b031d9e22
wanglinglong <wanglinglong@boohee.com>
2015-11-02 16:31:30 +0800

    add ZigZag

---
+++ ZigZagConversion.java
@@ -0,0 +1,38 @@
+/**
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+ */
+public class Solution {
+    public String convert(String s, int nRows) {
+        int len = s.length();  
+        if (len == 0 || nRows <= 1) return s;  
+          
+        String[] ans = new String[nRows];  
+        Arrays.fill(ans, "");  
+        int row = 0, delta = 1;  
+        for (int i = 0; i < len; i++) {  
+            ans[row] += s.charAt(i);  
+            row += delta;  
+            if (row >= nRows) {  
+                row = nRows-2;  
+                delta = -1;  
+            }  
+            if (row < 0) {  
+                row = 1;  
+                delta = 1;  
+            }  
+        }  
+          
+        String ret = "";  
+        for (int i = 0; i < nRows; i++) {  
+            ret += ans[i];  
+        }  
+        return ret;  
+    }
+}
